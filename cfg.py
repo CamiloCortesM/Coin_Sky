dcdbc2333dc7eaa27903c988ab25238d395af79f
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-01-27 23:16:21 -0500

    food class

--- cfg.py
+++ cfg.py
@@ -4,10 +4,10 @@ import os
 
 
 IMAGE_PATHS ={
-    'gold': os.path.join(os.getcwd,'resources','images','gold.png'),
-    'apple': os.path.join(os.getcwd,'resources','images','apple.png'),
-    'background': os.path.join(os.getcwd,'resources','images','background.jpg'),
-    'hero': [os.path.join(os.getcwd,'resources','images','%d.png' % i) for i in range(1,11))]
+    'gold': os.path.join(os.getcwd(),'resources','images','gold.png'),
+    'apple': os.path.join(os.getcwd(),'resources','images','apple.png'),
+    'background': os.path.join(os.getcwd(),'resources','images','background.jpg'),
+    'hero': [os.path.join(os.getcwd(),'resources','images','%d.png' % i) for i in range(1,11)]
 }
 
 AUDIO_PATHS = {
